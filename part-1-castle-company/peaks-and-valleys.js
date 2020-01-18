99eb2ec57f52f581f6374e37b456084c6df1dec1
Kelly Waters <kellywaters@gmail.com>
2020-01-18 09:23:07 -0800

    Initialize NPM Project

--- part-1-castle-company/peaks-and-valleys.js
+++ part-1-castle-company/peaks-and-valleys.js
@@ -1,11 +1,11 @@
-const peaksAndValleys = arr => {
+export const peaksAndValleys = arr => {
     let totalCastles = 0
     let prevVal = 0
     let currVal = 0
     for (let i = 1; i <= arr.length; i++) {
         prevVal = arr[i-1]
         currVal = arr[i]
-        if (arr[i] !== arr[i-1]) {
+        if (currVal !== prevVal) {
             if (currVal - prevVal !== 0) {
                 totalCastles +=1
             }
@@ -19,4 +19,4 @@ const peaksAndValleys = arr => {
 }
 
 console.log(peaksAndValleys([1,2,2,2,2,3,4,5,6,3,26,7,7,7,7,7,2]))
-console.log(peaksAndValleys([6, 1,2, 1, 4, 4, 4]))
+console.log(peaksAndValleys([6, 1, 2, 1, 4, 45, 4]))
